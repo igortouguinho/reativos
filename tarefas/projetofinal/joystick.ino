5f07eb5d265b9dca7d5079cc6919a87dbe0c8f9b
igortouguinho <igor_tss@hotmail.com>
2017-06-28 08:42:28 -0300

    Update joystick.ino

--- tarefas/projetofinal/joystick.ino
+++ tarefas/projetofinal/joystick.ino
@@ -1,9 +1,9 @@
-
 // Joystick x- and y- axes are connected to A10 and A9 analog pins of Arduino.
 
-int joyX = A10; //  X - axis of Joystick
-int joyY = A9; // y - axis of Joystick
-int x, y, dir; 
+int joyX = A5; //  X - axis of Joystick
+int joyY = A4; // y - axis of Joystick
+int x, y; 
+char dir;
 
 void setup()
 { 
@@ -18,32 +18,32 @@ void loop()
   x = map(analogRead(joyY), 0, 1023, 900, 2100);
   
   if ( x > 1400 && x < 1600 && y > 1400 && y < 1600){
-    dir = 0;
+    dir = 'S';
   }
   if ( x > 1400 && x < 1600 && y > 2000 && y < 2200){
-    dir = 1;
+    dir = 'W';
   }
   if ( x > 2000 && x < 2200 && y > 1400 && y < 1600){
-    dir = 2;
+    dir = 'D';
   }
   if ( x > 1400 && x < 1600 && y > 800 && y < 1000){
-    dir = 3;
+    dir = 'X';
   }
   if ( x > 800 && x < 1000 && y > 1400 && y < 1600){
-    dir = 4;
+    dir = 'A';
   }
   if ( x > 800 && x < 1200 && y > 2000 && y < 2200){
-    dir = 5;
+    dir = 'Q';
   }
   if ( x > 2000 && x < 2200 && y > 2000 && y < 2200){
-    dir = 6;
+    dir = 'E';
   }
   if ( x > 2000 && x < 2200 && y > 800 && y < 1200){
-    dir = 7;
+    dir = 'C';
   }
   if ( x > 800 && x < 1200 && y > 800 && y < 1200){
-    dir = 8;
+    dir = 'Z';
   }
   
-  Serial.println(dir);
+  Serial.write(dir);
 }
